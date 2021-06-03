1d4982013e7c935c92a6b27bc7babe77eeb57d8c
Reed Cowan <reed.cowan@Reeds-MBP-2.attlocal.net>
2021-06-02 20:13:36 -0400

    restructured directories

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,11 +1,14 @@
 package app
 
-import "bookstore_items-api/controllers"
+import (
+	"bookstore_items-api/controllers/ping"
+	"bookstore_items-api/controllers/users"
+)
 
 func mapUrls() {
-	r.GET("/ping", controllers.Ping)
+	r.GET("/ping", ping.Ping)
 
-	r.GET("/users/:user_id", controllers.GetUser)
-	// r.GET("/users/search", controllers.SearchUser)
-	r.POST("/users", controllers.CreateUser)
+	r.GET("/users/:user_id", users.GetUser)
+	// r.GET("/users/search", users.SearchUser)
+	r.POST("/users", users.CreateUser)
 }
--- controllers/ping/ping_controller.go
+++ controllers/ping/ping_controller.go
@@ -1,4 +1,4 @@
-package controllers
+package ping
 
 import (
 	"net/http"
